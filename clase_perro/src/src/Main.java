4f0c470cc7ef16a959e99db96b2778b522866b83
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2025-04-04 19:39:03 -0700

    lets add dog class

---
+++ clase_perro/src/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Perro newDog = new Perro();
+        newDog.showMsg();
+    }
+}
---
+++ clase_perro/src/src/Perro.java
@@ -0,0 +1,12 @@
+public class Perro {
+    private String name = "doggy";
+    private int age = 15;
+
+    public Perro(){
+
+    }
+
+    public void showMsg(){
+        System.out.println("name: " + this.name + ", age: " + this.age);
+    }
+}
